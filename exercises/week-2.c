e768c364929820ce1edc98a4424e6209151f84eb
Fernando Pacheco <fpacheco0707@yahoo.com>
2019-09-19 19:56:46 -0700

    exercises: Add exercises for week 2 session
    Signed-off-by: Fernando Pacheco <fpacheco0707@yahoo.com>

---
+++ exercises/week-2.c
@@ -0,0 +1,19 @@
+/*
+ * Exercises:
+ *	1. Create a function that swaps the values of two integers.
+ *
+ *	2. Create your own version of the strlen function using
+ *	   only pointer arithmetic. You may use the strlen man
+ *	   page for a description of the function:
+ *	   http://man7.org/linux/man-pages/man3/strlen.3.html
+ *
+ *	3. Write a small program that declares and initializes a
+ *	   char and a pointer to this char. Your program should
+ *	   print out the value, address, and size of each of the
+ *	   two variables.
+ *
+ *	4. Generate a patch using git-format-patch [1]. Make sure
+ *	   to include your signed-off by (-s option).
+ *
+ *	   [1] https://git-scm.com/docs/git-format-patch
+ */
