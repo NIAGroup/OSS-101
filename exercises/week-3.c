1d07afa386bf330dae5eaa8bc9524eef62c91f22
Fernando Pacheco <fpacheco0707@yahoo.com>
2019-09-26 22:27:17 -0700

    exercises: week 3
    Signed-off-by: Fernando Pacheco <fpacheco0707@yahoo.com>

---
+++ exercises/week-3.c
@@ -0,0 +1,18 @@
+/*
+ * Exercises:
+ *	1. Create your own version of the memcpy function using
+ *	   only pointer arithmetic. You may use the memcpy man
+ *	   page for a description of the function [1].
+ *
+ *	2. Use the qsort [2] function to sort an array of integers
+ *	   in ascending or descending order by creating two functions
+ *	   that can be used as the last argument to qsort.
+ *
+ *	3. Implement a singly linked list [3] along with functions
+ *	   to add an element to the end of the list, delete an element,
+ *	   and traverse a list and print its elements.
+ *
+ *	[1] http://man7.org/linux/man-pages/man3/memcpy.3.html
+ *	[2] http://man7.org/linux/man-pages/man3/qsort.3p.html
+ *	[3] https://www.cs.cmu.edu/~adamchik/15-121/lectures/Linked%20Lists/linked%20lists.html
+ */
